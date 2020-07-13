090f59338042fbc809553eb604292f01d4d9cec8
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-14 00:11:51 +0900

    done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,16 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+    }
+    if n > 2 && m > 2 {
+        println!("{}", (n - 2) * (m - 2));
+    } else if n == 2 || m == 2 {
+        println!("0");
+    } else if n == 1 && m == 1 {
+        println!("1");
+    } else {
+        println!("{}", n * m - 2);
     }
 }
